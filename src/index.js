c2518545eaab169291b63394fbce0539834bc559
Wakhata <wakhata@USERs-Air.lan>
2021-02-27 08:15:19 +0300

    End of section 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./app/layout/styles.css";
+import App from "./app/layout/App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootEl = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+function render() {
+  ReactDOM.render(<App />, rootEl);
+}
+
+if (module.hot) {
+  module.hot.accept("./app/layout/App", function () {
+    setTimeout(render);
+  });
+}
+
+render();
+
 reportWebVitals();
